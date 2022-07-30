7422bed3f253687412bef2afe00225716c4bad61
Brigitte Rico <bricogalindo@gmail.com>
2022-07-29 23:02:16 -0500

    Nueva dependencia Story Book instalada

---
+++ src/components/blank/Blank.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="blank" :style="{ backgroundColor }">
+    Example Blank
+    <!-- Template here -->
+  </div>
+</template>
+<script>
+// Script here
+export default {
+  props: {
+    backgroundColor: String,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+// Style here
+</style>
